583228b526cb1f372f6e58ddd8153fd1134d7b04
Pradeep Verghese <p.verghese@gmail.com>
2024-05-08 04:14:41 +0000

    Added boutique suggestions

---
+++ boutique-suggestions/lib/boutique_suggestions.ex
@@ -0,0 +1,11 @@
+defmodule BoutiqueSuggestions do
+  def get_combinations(tops, bottoms, options \\ []) do
+    max_price = Keyword.get(options, :maximum_price, 100.0)
+    for t <- tops,
+      b <- bottoms,
+      t.base_color != b.base_color,
+      t.price + b.price <= max_price  do
+        {t,b}
+      end
+  end
+end
